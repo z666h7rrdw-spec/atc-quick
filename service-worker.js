b048f28774f755b1d663c7db61aa67c763d3c1da
z666h7rrdw-spec <z666h7rrdw@privaterelay.appleid.com>
2026-09-19 05:41:25 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,10 @@
-const C='atc-quick-v35';const F=['./','./index.html','./data.js','./airports.json','./manifest.json'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='atc-quick-v36';
+const F=['./','./index.html','./data.js','./airports.json','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(F)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))),self.clients.claim()])));
+self.addEventListener('fetch',e=>{
+ if(e.request.mode==='navigate'){
+  e.respondWith(fetch(e.request).then(r=>{let x=r.clone();caches.open(C).then(c=>c.put('./index.html',x));return r}).catch(()=>caches.match('./index.html')));return;
+ }
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(n=>{let x=n.clone();caches.open(C).then(c=>c.put(e.request,x));return n})));
+});
