bf30ad91ebbdd799e95cc7f460f3c358163406c6
z666h7rrdw-spec <z666h7rrdw@privaterelay.appleid.com>
2026-09-26 11:12:42 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const C='atc-quick-v89';
+const C='atc-quick-v90';
 const F=['./index.html','./data.js?v=82','./airports.json','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',e=>{
